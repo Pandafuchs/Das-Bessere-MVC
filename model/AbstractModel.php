8cd2a1ecf33a90824db388da4483f1ad3c1c5e18
Pandafuchs <github@notzon.me>
2015-05-28 10:56:17 +0200

    Update and rename abstractmodel.php to AbstractModel.php

--- model/AbstractModel.php
+++ model/AbstractModel.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * light weight php mvc-framework
- *
- * https://github.com/ReWrite94/lightweight-php-mvc-framework
-*/
-
 require_once 'class/pdosingleton.php';
 
 abstract class AbstractModel {
