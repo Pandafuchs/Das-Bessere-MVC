d3dd795bcb4729f51248577e9d01edf494254fb0
Pandafuchs <github@notzon.me>
2016-04-01 08:59:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <?php
-//error_reporting(E_NONE);
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
-session_start();
-
-//Konstanten
-define('SALT', 'RANDOM_SALT!');
-
+//Das Hauptprogramm starten
+include_once 'model/Logtable.php';
+include_once 'model/nachricht.php';
+include_once 'include/settings.php';  
+include_once 'include/error_handling.php';
 include_once 'class/template.php'; 
-include_once 'controller/FrontController.php';
+include_once 'controller/FrontController.php'; 
 
 $template = new Template();
-
 $frontController = new FrontController($template);
-date_default_timezone_set('Europe/Berlin');
-setlocale (LC_ALL, 'de_DE');
-
-$frontController->execute();
+$frontController->execute();  
 
 ?>
